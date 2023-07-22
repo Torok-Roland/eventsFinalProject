bc56b41484aa2a6a5173ddf52a986c7d6532bd15
Torok Roland <torok.roland1998@gmail.com>
2023-07-22 16:09:15 +0300

    Read, update was created!

--- src/main/java/ro/sda/eventsFinalProject/controller/EventController.java
+++ src/main/java/ro/sda/eventsFinalProject/controller/EventController.java
@@ -2,10 +2,11 @@ package ro.sda.eventsFinalProject.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ro.sda.eventsFinalProject.model.Event;
 import ro.sda.eventsFinalProject.service.EventService;
+
+import java.util.List;
 
 @RestController
 public class EventController {
@@ -16,12 +17,42 @@ public class EventController {
 
     }
 
+    @PostMapping("/events")
     public ResponseEntity createEvent(@RequestBody Event event) {
-        try{
+        try {
             Event savedEvent = eventService.saveEvent(event);
             return new ResponseEntity<>(savedEvent, HttpStatus.OK);
-        } catch (IllegalArgumentException iAE){
+        } catch (IllegalArgumentException iAE) {
             return new ResponseEntity(iAE.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/events/{id}")
+    public ResponseEntity readEvent(@PathVariable(name = "id") Integer eventId) {
+        try {
+            Event readEvent = eventService.readEvent(eventId);
+            return new ResponseEntity(readEvent, HttpStatus.OK);
+        } catch (IllegalArgumentException iAE) {
+            return new ResponseEntity(iAE.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/events")
+    public ResponseEntity readAllEvents() {
+        List<Event> allEvents = eventService.readAllEvents();
+        return new ResponseEntity(allEvents, HttpStatus.OK);
+    }
+
+    @PutMapping("/events/{id}")
+    public ResponseEntity update(@PathVariable(name = "id") Integer pathId, @RequestBody Event updatedEvent) {
+        if (!pathId.equals(updatedEvent.getId())) {
+            return new ResponseEntity("Inconsistent ID", HttpStatus.BAD_REQUEST);
+        }
+        try {
+            Event updated = eventService.eventUpdate(updatedEvent);
+            return new ResponseEntity<>(updated, HttpStatus.OK);
+        } catch (IllegalArgumentException iAE){
+            return new ResponseEntity<>(iAE.getMessage(),HttpStatus.BAD_REQUEST);
         }
     }
 
--- src/main/java/ro/sda/eventsFinalProject/service/EventService.java
+++ src/main/java/ro/sda/eventsFinalProject/service/EventService.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service;
 import ro.sda.eventsFinalProject.model.Event;
 import ro.sda.eventsFinalProject.repository.EventRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class EventService {
 
@@ -14,17 +17,43 @@ public class EventService {
 
     }
 
-    public Event saveEvent(Event eventToBeSaved){
-        if(eventToBeSaved.getName() == null){
+    public Event saveEvent(Event eventToBeSaved) {
+        if (eventToBeSaved.getName() == null) {
             // Avoids the generation of NullPointerException for null event names!
             throw new IllegalArgumentException("An event must have a name");
         }
         if (eventToBeSaved.getStartDate() == null || eventToBeSaved.getEndDate() == null ||
-                eventToBeSaved.getStartDate().isAfter(eventToBeSaved.getEndDate())){
+                eventToBeSaved.getStartDate().isAfter(eventToBeSaved.getEndDate())) {
             throw new IllegalArgumentException("Start date is after end date. Please be careful");
         }
         Event savedEvent = eventRepository.save(eventToBeSaved);
         return savedEvent;
+    }
+
+    public Event readEvent(Integer id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Events id must not be null!");
+        }
+        // orElse (object the OtherValue)
+        // if (this.value != null
+        //     return this.value
+        // else
+        //     return theOtherValue
+        Event event = eventRepository.findById(id).orElse(null);
+        if (event == null) {
+            throw new IllegalArgumentException("There it is no event with " + id);
+        }
+        return event;
+    }
+
+    public List<Event> readAllEvents(){
+        return eventRepository.findAll();
+    }
+
+    public Event eventUpdate(Event updatedEvent){
+           Event eventToUpdate = readEvent(updatedEvent.getId());
+           eventRepository.save(updatedEvent);
+           return eventToUpdate;
     }
 
 }
